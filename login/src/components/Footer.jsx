74f6f352f73147209d96146d047dbe4c467a9065
Leonardo Coelho Moraes <108236132+Leonardo-Coelho-Moraes@users.noreply.github.com>
2023-03-05 12:14:30 -0400

    oi

--- login/src/components/Footer.jsx
+++ login/src/components/Footer.jsx
@@ -6,39 +6,39 @@ import Style from '../styles/Footer.module.css'
 const Footer = () => {
   return (
     <footer className={Style.footer}>
-    <ul>
-      <li>
+    <div className={Style.ul}>
+      <div className={Style.li}>
         <a href="#">
           <TbPower/>
           <h2>Jogue também offline</h2>
         </a>
-      </li>
-      <li>
+      </div>
+      <div className={Style.li}>
         <a href="#">
           <TbUser/>
           <h2>67 jogadores</h2>
         </a>
-      </li>
-      <li>
+      </div>
+      <div className={Style.li}>
         <a href="#">
           <TbDeviceGamepad2/>
           <h2>Compatível com uso remoto</h2>
         </a>
-      </li>
-      <li>
+      </div >
+      <div className={Style.li}>
         <a href="#">
           <BsPhoneVibrate/>
           <h2>Vibração no controle</h2>
         </a>
-      </li>
-      <li>
+      </div >
+      <div className={Style.li}>
         <a href="#">
           <TbMoodKid/>
           <h2>Permitido para menores</h2>
         </a>
-      </li>
+      </div>
       
-    </ul>
+    </div>
     
     </footer>
   )
--- login/src/components/Navbar.jsx
+++ login/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { React, useState} from 'react'
 import Logo from '../imgs/logo.png'
 import Style from '../styles/Navbar.module.css'
 import Ripples from 'react-ripples'
 const Navbar = () => {
+  const [IsMenuActive, setIsMenuActive] = useState(true)
+
+  
+
   return (
     <>
     <header className={Style.header}>
@@ -10,7 +14,7 @@ const Navbar = () => {
       <img src={Logo} alt="Logo Mine" /> <h1>MINE</h1>
       
     </a>
-    <ul>
+    <ul className={` ${!IsMenuActive? 'active' : ''}`}>
         
         <li><a href='#'>
           Fórum</a></li>
@@ -24,6 +28,11 @@ const Navbar = () => {
             <button>Entrar</button>
           </Ripples>
       </ul>
+      <div className={`mobile-menu ${!IsMenuActive? 'active' : ''}`} onClick={() => {!IsMenuActive? setIsMenuActive(true) : setIsMenuActive(false)}}>
+      <div className='line1'></div>
+      <div className='line2'></div>
+      <div className='line3'></div>
+      </div>
     </header>
     
     </>
